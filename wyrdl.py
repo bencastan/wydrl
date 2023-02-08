ffa00f21ec4511450761debd62d184200805afc8
Ben Castan <ben@castanfamily.com>
2023-02-08 11:45:43 +1100

    Typos

--- wyrdl.py
+++ wyrdl.py
@@ -15,5 +15,5 @@ for guess_num in range(1,7):
     wrong_letters = set(guess) - set(WORD)
 
     print("Correct letters:", ", ".join(sorted(correct_letters)))
-    print("Missplaced letters:", ", ".join(sorted(misplaced_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
