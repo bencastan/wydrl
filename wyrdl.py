7478c6ec5132fe4056fe4ea9dfe8af066a2a7572
Ben Castan <ben@castanfamily.com>
2023-02-08 13:51:34 +1100

    Choose a Random Word From a Word List

--- wyrdl.py
+++ wyrdl.py
@@ -1,19 +1,32 @@
 # wyrdl.py
 
-WORD = "SNAKE"
+import pathlib
+import random
+
+WORDLIST = pathlib.Path("wordlist.txt")
+
+words = [
+    word.upper()
+    for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
+]
+word = random.choice(words)
+
 
 for guess_num in range(1,7):
     guess = input (f"\nGuess {guess_num}: ").upper()
-    if guess == WORD:
+    if guess == word:
         print("Correct")
         break
         
     correct_letters = {
-        letter for letter, correct in zip(guess, WORD) if letter == correct
+        letter for letter, correct in zip(guess, word) if letter == correct
     }
-    misplaced_letters = set(guess) & set(WORD) - correct_letters
-    wrong_letters = set(guess) - set(WORD)
+    misplaced_letters = set(guess) & set(word) - correct_letters
+    wrong_letters = set(guess) - set(word)
 
     print("Correct letters:", ", ".join(sorted(correct_letters)))
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+
+else:
+    print(f"The word was: {word}")
