d7c20c9108ea166d97ea8631b336f47025cb3d4c
Ben Castan <ben@castanfamily.com>
2023-02-12 12:38:01 +1100

    After refactoring

--- wyrdl.py
+++ wyrdl.py
@@ -4,22 +4,61 @@ import pathlib
 import random
 from string import ascii_letters
 
-WORDLIST = pathlib.Path("wordlist.txt")
+# WORDLIST = pathlib.Path("wordlist.txt")
 
-words = [
-    word.upper()
-    for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
-    if len(word) ==5 and all(letter in ascii_letters for letter in word)
-]
-word = random.choice(words)
+# words = [
+#     word.upper()
+#     for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
+#     if len(word) ==5 and all(letter in ascii_letters for letter in word)
+# ]
+# word = random.choice(words)
 
 
-for guess_num in range(1,7):
-    guess = input (f"\nGuess {guess_num}: ").upper()
-    if guess == word:
-        print("Correct")
-        break
+# for guess_num in range(1,7):
+#     guess = input (f"\nGuess {guess_num}: ").upper()
+#     if guess == word:
+#         print("Correct")
+#         break
         
+#     correct_letters = {
+#         letter for letter, correct in zip(guess, word) if letter == correct
+#     }
+#     misplaced_letters = set(guess) & set(word) - correct_letters
+#     wrong_letters = set(guess) - set(word)
+
+#     print("Correct letters:", ", ".join(sorted(correct_letters)))
+#     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+#     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+
+# else:
+#     print(f"The word was: {word}")
+
+def main():
+    # Pre-process
+    word = get_random_word()
+
+    # Process (main loop)
+    for guess_num in range(1,7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
+
+        show_guess(guess, word)
+        if guess == word:
+            break
+
+    # Post-process
+    else:
+        game_over(word)
+
+def get_random_word():
+    wordlist = pathlib.Path(__file__).parent / "wordlist.txt"
+    words = [
+        word.upper()
+        for word in wordlist.read_text(encoding="utf-8").split("\n")
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+    ]
+    return random.choice(words)
+
+def show_guess(guess, word):
     correct_letters = {
         letter for letter, correct in zip(guess, word) if letter == correct
     }
@@ -30,5 +69,8 @@ for guess_num in range(1,7):
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
 
-else:
-    print(f"The word was: {word}")
+def game_over(word):
+    print(f"The word was {word}")
+
+if __name__ == "__main__":
+    main()
