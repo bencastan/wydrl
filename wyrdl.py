db79c63ec24d6ac7a0d9236eeed6eebc28b331c8
Ben Castan <ben@castanfamily.com>
2023-02-16 11:59:30 +1100

    Step 4

--- wyrdl.py
+++ wyrdl.py
@@ -3,6 +3,10 @@
 import pathlib
 import random
 from string import ascii_letters
+from rich.console import Console
+from rich.theme import Theme
+
+console = Console(width=40, theme=Theme({"warning": "red on yellow"}))
 
 # WORDLIST = pathlib.Path("wordlist.txt")
 
@@ -37,21 +41,24 @@ def main():
     # Pre-process
     words_path = pathlib.Path(__file__).parent / "wordlist.txt"
     word = get_random_word(words_path.read_text(encoding="utf-8").split("\n"))
+    guesses = ["_" * 5] * 6
 
     # Process (main loop)
-    for guess_num in range(1,7):
-        guess = input(f"\nGuess {guess_num}: ").upper()
+    for idx in range(6):
+        refresh_page(headline=f"Guess {idx + 1}")
+        show_guesses(guesses, word)
 
-        show_guess(guess, word)
-        if guess == word:
+        guesses[idx] = input(f"\nGuess word: ").upper()
+        if guesses[idx] == word:
             break
 
     # Post-process
-    else:
-        game_over(word)
+    # Remove else
+    
+    game_over(guesses, word, guessed_correctly=guesses[idx] == word)
 
 def get_random_word(word_list):
-    """Get a random five letter word froma list of strings.
+    """Get a random five letter word from a list of strings.
     
     ## Example:
     
@@ -85,8 +92,33 @@ def show_guess(guess, word):
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
 
-def game_over(word):
-    print(f"The word was {word}")
+def show_guesses(guesses, word):
+    for guess in guesses:
+        styled_guess = []
+        for letter, correct in zip(guess, word):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in word:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "white on #666666"
+            else:
+                style ="dim"
+            styled_guess.append(f"[{style}]{letter}[/]")
+        console.print("".join(styled_guess), justify="center")
+
+def refresh_page(headline):
+    console.clear()
+    console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")    
+    
+def game_over(guesses, word, guessed_correctly):
+    refresh_page(headline="Game Over")
+    show_guesses(guesses, word)
+
+    if guessed_correctly:
+        console.print(f"\n[bold white on green]Correct, the word is {word} [/]")
+    else:
+        console.print(f"\n[bold white on red]Sorry the word was {word}[/]")
 
 if __name__ == "__main__":
     main()
